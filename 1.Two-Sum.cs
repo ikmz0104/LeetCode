6178188996bbeb418b4c17054921070fd0f062b7
ikmz <ikmz0104.official@gmail.com>
2021-12-01 23:56:11 +0900

    条件分岐

--- 1.Two-Sum.cs
+++ 1.Two-Sum.cs
@@ -1,4 +1,20 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
+        //解答用
+        Dictionary<int, int> ans = new Dictionary<int, int>();
+        //差分
+        int left_num = target - nums[i]; 
+        
+        //差分の検知
+        if()
+        {
+            
+        }
+        else if()
+        {
+            
+        }
+        return null;
+        
     }
 }
