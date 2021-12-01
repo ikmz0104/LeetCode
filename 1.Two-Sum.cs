f7d0d653d23f4149d785b23edf98fa861dcdb1fa
ikmz <ikmz0104.official@gmail.com>
2021-12-02 00:16:29 +0900

    ウェイよ

--- 1.Two-Sum.cs
+++ 1.Two-Sum.cs
@@ -2,19 +2,23 @@ public class Solution {
     public int[] TwoSum(int[] nums, int target) {
         //解答用
         Dictionary<int, int> ans = new Dictionary<int, int>();
-        //差分
-        int left_num = target - nums[i]; 
-        
-        //差分の検知
-        if()
+
+        for (int i=0; i<nums.Length; i++)
         {
+            //差分
+            int left_num = target - nums[i]; 
             
-        }
-        else if()
-        {
-            
+            //差分の検知
+            if(ans.ContainsKey(left_num))
+            {
+                return new int[] {ans[left_num], i};
+            }
+            else if(!ans.ContainsKey(nums[i]))
+            {
+                ans.Add(nums[i], i);
+            }
+
         }
         return null;
-        
     }
 }
