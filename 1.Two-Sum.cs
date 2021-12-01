03fc781c989418adb02482a50b69428605002332
ikmz <ikmz0104.official@gmail.com>
2021-12-01 23:51:56 +0900

    ロジックおさらいと組み立て

--- 1.Two-Sum.cs
+++ 1.Two-Sum.cs
@@ -1,22 +1,4 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        //var オブジェクト名 = new Dictionary<Keyの型名, Valueの型名>()
-        var ans = new Dictionary<int, int>();
-        for (int i = 0; i < nums.Length(); i++)
-        {
-            //nums[i], nums[i+1]
-            var Diff = target - nums[i];
-            
-            //DictionaryでDiffを見つける
-            if(ans.ContainsValue(Diff))
-            {
-                return new int[]();　//ここ書けるようにまた明日
-            }
-            else
-            {
-                ans.add(i, nums[i]);
-            }
-            return null;
-        }
     }
 }
