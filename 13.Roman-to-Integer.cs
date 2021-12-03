1046d6a5f49c99f255dc4fe31d9f308e83b22ffd
ikmz <ikmz0104.official@gmail.com>
2021-12-03 13:18:32 +0900

    WIP: 動的と静的に分割した

--- 13.Roman-to-Integer.cs
+++ 13.Roman-to-Integer.cs
@@ -1,32 +1,54 @@
 public class Solution {
-    public int RomanToInt(string s) {
+    //動的返却
+    public int RomanToInt(char roman) {
         
-        //ハッシュテーブルの宣言
-        Dictionary<string, int> KeyValueData = new Dictionary<string, int>();
+        int ans1 = 0;
+        int ans2 = 0;
         
-        //値のセット
-        {'I', 1};
-        {'V', 5};
-        {'X', 10};
-        {'L', 50};
-        {'C', 100};
-        {'D', 500};
-        {'M', 1000};
-        
-        //値を取り出す
-        int Value = KeyValueData["Key1"];
-        
-        //返却される値をansに加算していく
-        int ans = 0;
-        
-        //1文字目と2文字目のValueを比較
-        for (int i=0; i<s.Length; i++)
+        for(int i=0; i<roman.Length; i++)
         {
-            if(s[i] < s[i+1])
+            //文字列のi番目のValue < i+1番目のValue　なら差
+            if (roman[i] < roman[i+1])
             {
-                ans = V - I;
-                return ans;
+                
             }
+            //文字列のi番目のValue > i+1番目のValue　なら和
+            else
+            {
+
+            }
+        }        
+
+    }
+    
+    //静的返却
+    static int StaticRomanToInt(char roman) {
+        switch(roman)
+        {
+            case 'I':
+                return 1;
+                break;
+            case 'V':
+                return 5;
+                break;
+            case 'X':
+                return 10;
+                break;
+            case 'L':
+                return 50;
+                break;
+            case 'C':
+                return 100;
+                break;
+            case 'D':
+                return 500;
+                break;
+            case 'M':
+                return 1000;
+                break;
+            default:
+                Console.WriteLine("defaultが実行されました");
+                break;
         }
     }
-}
+} 
